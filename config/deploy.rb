0cb0ca81fd78fb378772dc827aef4d1d9d5b483e
AldanaQuintana <aldanaqm@gmail.com>
2019-06-17 17:34:03 -0300

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,19 +2,14 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'foreman'
 
-if ENV['RACK_ENV'] == 'production'
-  set :domain, ''
-  set :deploy_to, ''
-  set :user, ''
-else
-  set :domain, ''
-  set :deploy_to, ''
-  set :user, ''
-end
-
-set :repository, ''
+set :domain, ''
+set :deploy_to, '/var/www/spotify-augmentor'
+set :user, 'ubuntu'
+set :repository, 'git@github.com:AldanaQuintana/spotify-augmentor-api.git'
 set :branch, 'master'
+set :env_config, File.open('./.env')
 
 set :shared_paths, ['tmp/server', 'log', '.rbenv-vars']
 
@@ -24,7 +19,23 @@ task :environment do
   invoke :'rbenv:load'
 end
 
+task :copy_env_config => :environment do
+  queue('echo "-----> Configuring environment"')
+  queue("echo \"\" > #{deploy_to}/#{shared_path}/.env")
+  env_config.each_line do |line|
+    queue("echo \"#{line.strip}\" >> #{deploy_to}/#{shared_path}/.env")
+  end
+  queue("echo \"PATH=$(bundle show bundler):$PATH\" >> #{deploy_to}/#{shared_path}/.env")
+  queue("echo \"ENVIRONMENT=production\" >> #{deploy_to}/#{shared_path}/.env")
+end
+
 task :setup => :environment do
+  queue("ln -sTf #{deploy_to}/#{current_path} /var/www/app")
+  queue("mkdir -p #{deploy_to}/#{shared_path}/log")
+end
+
+task :export_foreman_jobs => :environment do
+  queue("cd #{deploy_to}/#{current_path} && sudo -E env \"PATH=$PATH\" foreman export upstart /etc/init -a queue -e #{deploy_to}/#{shared_path}/.env")
 end
 
 desc "Deploys the current version to the server."
@@ -37,6 +48,13 @@ task :deploy => :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'deploy:cleanup'
+    invoke :'copy_env_config'
+
+    to :launch do
+      invoke :'export_foreman_jobs'
+      invoke :'server:restart'
+      invoke :'server:restart_queue'
+    end
   end
 end
 
@@ -65,4 +83,9 @@ namespace :server do
     invoke :'server:start'
   end
 
+  task :restart_queue => :environment do
+    queue %Q%
+      sudo start queue || sudo restart queue
+    %
+  end
 end
